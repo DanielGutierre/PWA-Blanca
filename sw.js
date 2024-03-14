d5e4da2a29f1521157263bbbfd4864ebca14f985
DanielGutierre <earody4@gmail.com>
2024-03-13 21:26:44 -0600

    Se actualizo sw y manifest

--- sw.js
+++ sw.js
@@ -1,70 +1,28 @@
-//Asignar nombre y version de la cache
-const CACHE_NAME='v1_cache_BCH_PWA';
+const STATIC_CACHE = "static";
 
-//configuración de los fichros a subir a la cache de la aplicación.
-var urlsToCache= [
-    './',
-    './css/style.css',
-    './imagenes/rines_chidos.jpg'
+const APP_SHELL = [
+    "/",
+    "Index.html",
+    "css/style.css",
+    "main.js",
+    "imagenes/rines_chidos.jpg",
 ];
-//Evento del ServerWorker
-//Evento Install
-//se encarga de la instalacion del SW
-//guarda en cache los recursos estáticos
-//a variable self permite recoger del SW
 
-self.addEventListener('Install', e=>{
-    //utilizamos la variable del evento
+self.addEventListener("install", (e) => {
+    const cacheStatic = caches
+    .open(STATIC_CACHE)
+    .then((cache) => cache.addAll(APP_SHELL));
 
-    e.waitUntil(
-        caches.open(CACHE_NAME)
-              .then(cache => {
-                //le mandamos los elementos que tenemos en el array
-                return cache.addAll(urlsToCache)
-                            .then(()=>{
-                                self.skipWaiting();
-                            })
-              })
-              .catch(err=>console.log('No se ha registrado el cache' , err))
-    );
+    e.waitUntil(cacheStatic);
 });
 
-//Evento activable
-//este evento permite que la aplicacion funcione offline
-self.addEventListener('activate' ,e => {
-    const cacheWhitelist = [CACHE_NAME];
+self.addEventListener("fetch", (e) => {
+    console.log("fectch! ", e.request);
 
-    //que el evento espere a que termine de ejecutar
-    e.waitUntil(
-        caches.keys()
-            .then(cacheNames=>{
-                return Promise.all(
-                    cacheNames.map(cacheNames => {
-                        if(cacheWhitelist.indexOf(cacheName)== -1)
-                        {
-                            //borrar elementos que no se necesitan
-                            return cache.delete(cacheName);
-                        }
-                    })
-                );
-            })
-            .then(()=> {
-                self.clients.claim(); //activa la cache en el dispositivo.
-
-            })
-    );
-})
-
-self.addEventListener('fetch',e => {
     e.respondWith(
-        caches.match(e.request)
-        .then(res => {
-            if(res){
-                //devuelvo datos desde cache
-                return res;
-            }
-            return fetch(e.request);
-            //hago petición al servidor en caso de que no este en cache
-        })
+        caches
+        .match(e.request)
+        .then((res) => res || fetch(e.request))
+        .catch(console.log)
     );
 });
